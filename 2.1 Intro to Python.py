91735d8308512e9e4ff71d8b558569f7c736f42e
Inaya20133 <inayasachdeva10@gmail.com>
2026-04-25 12:38:18 -0400

    Add files via upload

---
+++ 2.1 Intro to Python.py
@@ -0,0 +1,16 @@
+#Examples
+# print("Hello World")
+# print("python can be used for printing")
+# print("Each new print statement is pirnted on a new line")
+# print("we have extra characters that have speical meaning too!")
+# print("/tbackslash t prints out a tab/ which is like sevral spaces")
+# print("backslash n/nprints a new line/neach time you type it/n!")
+
+#C.W
+#1
+print('Inaya')
+print("Inaya")
+print('April 25, 2026')
+print("Monday\n Tuesday\n Wednesday\n Thursday\n Friday\n Saturday\n Sunday\n")
+print("1\t 2\t 3\t 4\t 5\t 6\t 7\t 8\t")
+print("HOW MANY, HOW MUCH \tby Shel Silverstein\nHow many slams in an old screen door?\n\tDepends how loud you shut it.\n\How many slices in a bread?\n\tDepends how thin you cut it.\nHow much good inside a day?\n\tDepends how good you live 'em.\nHow much love inside a friend?\n\tDepends how much you give 'em")
