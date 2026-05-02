7b2a56ac550591435a1ad6a05fa35f9aff5655f8
Inaya20133 <inayasachdeva10@gmail.com>
2026-05-02 12:55:42 -0400

    Add files via upload

---
+++ 2.2.cw.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+print("Monday\n\tTuesday\n\t\tWednesday\n\t\t\tThurday\n\t\t\t\tFriday")
+print("Saturday\nSunday")
+
+print("1.allowed\n 2.not allowed\n 3.not allowed\n 4.not allowed\n 5.not allowed\n 6.not allowed\n 7.allowed\n 8.allowed\n 9.not allowed")
+#making intergers 
+import turtle 
+t= turtle.Turtle()
+angle= 90 
+angle2= -90
+t.left(angle)
+t.right(angle2)
+#making floats
+fwd= 10.5
+back = -99.9 
+t.forward(fwd)
+t.forward(back)
