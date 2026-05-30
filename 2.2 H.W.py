aaa752f1ecb903307f3cccaaed1c65435a69aae6
Inaya20133 <inayasachdeva10@gmail.com>
2026-05-30 12:41:17 -0400

    Add files via upload

---
+++ 2.2 H.W.py
@@ -0,0 +1,23 @@
+print("1.valid\n 2.valid\n 3. not valid\n 4.not valid\n 5.not valid\n 6. not valid\n 7.not valid\n 8.not valid\n 9. not valid")
+money_in_account = 7.50 # float
+value = 0.55 #float
+Print = "message" #string
+First_place_price = 10 #integers
+last_name = "Liu" #string
+one_updated_ = False #blooean
+Phyton = 0.00 #float
+
+steps = 150
+agl = 90
+color = "blue"
+import turtle
+shelly = turtle.Turtle()
+shelly.color(color)
+shelly.forward(steps)
+shelly.left(agl)
+shelly.forward(steps)
+shelly.left(agl)
+shelly.forward(steps)
+shelly.left(agl)
+shelly.forward(steps)
+shelly.left(agl)
