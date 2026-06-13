0c902e5394995ee707ce07c98e26faa03b48d449
Inaya20133 <inayasachdeva10@gmail.com>
2026-06-13 12:26:37 -0400

    Add files via upload

---
+++ 2.3c.w.py
@@ -0,0 +1,55 @@
+##movie_name = "Turing Red" 
+##year_released = 2022
+##rating = 4.6
+##best_band = "4- town"
+##print("The movie today we are watching is ",movie_name)
+##print(movie_name,"was relased in year",year_released)
+##
+##dogs_I_have = 2
+##dogs_I_wish_I_had = (dogs_I_have * 3) + 12
+##print("Dog I wish I had ",dogs_I_wish_I_had)
+##
+##money_in_pocket = 20.50
+##cost_of_hotdog = 1.99
+##money_leftover = money_in_pocket - (cost_of_hotdog * 6)
+##print("change in my pocket $",money_leftover)
+##
+####my_name = "Sam"
+####print(my_name - 5)
+##
+##x = 3 + 4 * 2 - 1
+##print(x)
+##
+##y = 6 * (2 + 1) - 5
+##print(y)
+##
+##name = input("Enter your name: ")
+##print("Welcome to world ", name)
+##
+##age= input("what is your age:")
+##print("i am", age)
+##
+##decimal= input("Enter a decimal number: ")
+##print(decimal)
+##
+##decimal = float(input("Enter a decimal number : "))
+##
+##x = float(input("Enter a decimal number: "))
+##print(x * 2)
+##
+##a = int(input("Enter a whole number: "))
+##b = int(input("Enter a whole number: "))
+##print(a + b)
+##
+##age_now = int(input("How old are you?"))
+##age_in_ten_years = age_now + 10
+##print("In ten years you will be", age_in_ten_years)
+
+##import turtle
+##distance = int(input("How long should the turtle move? "))
+##t=turtle.Turtle()
+##t.forward(distance)
+##turtle.done()
+
+username = input("Enter a username: ")
+print("Sorry, the username", username, "is taken")
