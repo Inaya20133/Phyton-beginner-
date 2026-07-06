f02384a293f8681a62cfb2cec08fbb6d9af1f465
Inaya20133 <inayasachdeva10@gmail.com>
2026-07-06 19:54:16 -0400

    Add files via upload

---
+++ 2.3hw.py
@@ -0,0 +1,65 @@
+###Q1
+##print("B= brackets\nE= expoents\nD= divison\nM= multiply\nA= additon\nS= subtraction")
+
+###Q2
+##print(10 * 3- 2)
+##
+##print(15 + 10 / 2 - 1)
+##
+##print((3 * 3) - (2 * 2 / 2))
+##
+##print(9 / (5 - 2) + 6)
+
+###Q3
+##a=int(input("write a number: "))
+##
+##b=int(input("write a number: "))
+##
+##c=int(input("write a number: "))
+##
+##witdh= a*b*c
+##
+##import turtle
+##
+##t=turtle.Turtle()
+##
+##t.forward(witdh)
+
+#Q4
+##a = "3005"
+##print(a,type(a))
+##
+##a=int(a)
+##print(a,type(a))
+##
+##b = "4.75"
+##print(b,type(b))
+##
+##b=float(b)
+##print(b,type(b))
+
+###Q5
+##a=float(input("write out a decimal number: "))
+##a=int(a)
+##b=float(input("write out a decimal number: "))
+##b=int(b)
+##c=float(input("write out a decimal number: " ))
+##c=int(c)
+##
+##import turtle
+##t=turtle.Turtle()
+##t.forward(a)
+##t.left(120)
+##t.forward(b)
+##t.left(120)
+##t.forward(c)
+
+###Q-Bonus
+##
+##a=input("enter a word: ")
+##b=input("enter a word: ")
+##print(a + b)
+##
+##a=input("enter a word: ")
+##b=int(input("enter a num: "))
+##print(a * b)
