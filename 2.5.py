3dd62f24e5ae864c48320b76ace06d0dd7d9c168
Inaya20133 <inayasachdeva10@gmail.com>
2026-09-05 12:55:16 -0400

    Add files via upload

---
+++ 2.5.py
@@ -0,0 +1,37 @@
+num1 = min(2.5, 0, -10, int("15"))
+num2 = max( 1, round(4.1), 12)
+print(sum((num1, num2)))
+
+
+print( 3 < 5)
+print( 2 == 2)
+
+print("meow" in "homeowner")
+print(4.5 != 4.5)
+print(0 <= 99 )
+print(-10 >= -10)
+
+
+name = input("What is your name?")
+print(name == "Jeremy")
+
+print( True and False)
+print( True or False)
+print( False and False)
+print( False or False)
+print( True or True)
+boolean = True and True
+
+
+print(12==12 and "cat" in "catastrophe")
+print(3 > 5 or 9 > 5)
+
+
+name = "samuel"
+age = 14
+print( "u" in name and age <10)
+
+
+print("j" in name or age ==14)
+
+
