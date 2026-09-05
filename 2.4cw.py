7c2497a278335272b4dc7fecb53ee7ce1c94cdb0
Inaya20133 <inayasachdeva10@gmail.com>
2026-09-05 12:11:43 -0400

    Add files via upload

---
+++ 2.4cw.py
@@ -0,0 +1,71 @@
+##year = int(input("What year is it now?"))
+##birth_year = int(input("what year were you born? "))
+##age = year - birth_year
+##print("you are probably" , age, "years old!")
+##
+##change_in_wallet = 15.65
+##birthday_money = 20.00
+##total_money = change_in_wallet + birthday_money
+##
+##a = 9 * 2 + 4/2
+##b = 20 + 10/5
+
+##import turtle
+##t = turtle.Turtle()
+##def draw_corner():
+##    t.forward(100)
+##    t.left(90)
+##    t.forward(100)
+##draw_corner()
+##
+##t = turtle.Turtle()
+##def draw_corner_2(size):
+##    t.forward(size)
+##    t.left(90)
+##    t.forward(size)
+##draw_corner_2(200)
+
+##name = input("Enter your name: ")
+##length_of_name = len(name)
+##print("your name has" , length_of_name, "letters in it!")
+##print("Your name has", len(name), "letters in it!")
+
+##a = int(input("Enter a number: "))
+##b = int(input("Enter a number: "))
+##c = int(input("Enter a number: "))
+##sum_of_all = sum((a,b,c))
+##print("The sum of your 3 numbers is", sum_of_all)
+
+
+##child_ticket_price = 3.99
+##adult_ticket_price = 8.99
+##senior_ticket_price = 4.99
+##cheapest_ticket = min(child_ticket_price, adult_ticket_price,
+##senior_ticket_price)
+##print("The lowest price for a ticket is", cheapest_ticket)
+##
+##
+##
+##biggest = max(24,2,13,-44)
+##print(biggest) 
+##
+##
+##num = float(input("Enter a decimal number: "))
+##print("The nearest whole number is: ", round(num))
+
+##print("a function is a instruction where the compunter will do a task like a additon or a subtraction")
+
+a=input("what is your name: ")
+len_a = len(a)
+b=input("what is your age: ")
+len_b = len(b)
+c=input("what is your eye color: ")
+len_c = len(c)
+totl=sum((len_a,len_b,len_c))
+print("the total characters in all three string is",totl)
+
+
+print("total of 1 to 10 is: ",sum((1,2,3,4,5,6,7,8,9,10)))
+
+
+subject1 = inpu(floatt("what is your mark"
