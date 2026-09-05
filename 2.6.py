9364e2463cf2b5425a3d9dd5ebeb8c7da2e73be8
Inaya20133 <inayasachdeva10@gmail.com>
2026-09-05 12:55:43 -0400

    Add files via upload

---
+++ 2.6.py
@@ -0,0 +1,86 @@
+##
+##my_birth_year = 2000
+##user_birth_year = int(input("What year were you born?"))
+##print("If I print True, you are older than me!")
+##print(my_birth_year < user_birth_year)
+##
+##
+##
+##my_favourite_word = "haberdashery"
+##your_favourite_word = input("What is your favourite word?")
+##print("If I print True, your word is longer!")
+##print(len(your_favourite_word) > len(my_favourite_word))
+##
+##
+##num1 = float(input("Enter a decimal number: "))
+##num2 = float(input("Enter a decimal number: "))
+##print("If I print True, both your numbers round to 4!")
+
+
+
+##user_birth_year = int(input("what is your birth year"))
+##if user_birth_year > 2000:
+##     print("I am older than you!")
+##elif user_birth_year == 2000:
+##     print("we are the same age!")
+##else:
+##     print("You are older than me!")
+##
+##my_favourtie_word = "pink"
+##your_favourtie_word =input("what is your favourtie word")
+##if len(my_favourtie_word) > len(your_favourtie_word):
+##    print("My word is longer!")
+##elif len(my_favourtie_word) < len(your_favourtie_word):
+##    print("your word is longer!")
+##else:
+##    print("our words have the same length!")
+##    print("They have", len(my_favourite_word),"letters")
+
+##word = "phyton"
+##letter = "y"
+##if letter in word:
+##    print("yes")
+##else:
+##    print("not in")
+
+
+
+##user_pet = input("What kind of pet do you have?")
+##if user_pet == "dog":
+##    print("Me too")
+##elif user_pet == "cat":
+##  print("I wish I could have a cat!")
+##elif user_pet == "bird":
+##  print("It would be so cool to have a bird.")
+##elif user_pet == "hamster" or user_input == "fish":
+##  print("I used to have a pet like that.")  
+##else: print("That's a unique pet!")
+
+
+##num = 3
+##if num < 10:
+## print("that's a single digit number!")
+##elif num == 16:
+##  print("thats my favourtie number,")
+##elif num < 13:
+##    print("my age is greater than that number.")
+##elif num == 10:
+##    print("there are 10 years in a decade.")
+##elif num <100:
+##    print("thats a 2 digit number")
+
+
+user_age =  12
+print(" you are very young")
+elif user_age = 12 :
+print("you are a teenager")
+elif user_age = 20:
+print("you are an adult")
+else user_age = <100:
+print("you are very old!")
+
+    
+     
+
+
+
